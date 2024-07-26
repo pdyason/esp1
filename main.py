446e57ea90490c6afb33b765614d80581acfdd00
pdyason <39290668+pdyason@users.noreply.github.com>
2024-07-26 02:08:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from machine import Pin
-from time import sleep
+from utime import sleep
+import _thread
 
 # Set the LED pin (usually GPIO2 for internal LED)
 led = Pin(2, Pin.OUT)
 
-while True:
-    led.on()   # Turn the LED on
-    sleep(0.5)   # Wait for 1 second
-    led.off()  # Turn the LED off
-    sleep(0.5)   # Wait for 1 second
+def loop():
+    while True:
+        led.on()   # Turn the LED on
+        sleep(0.2)   # Wait for 1 second
+        led.off()  # Turn the LED off
+        sleep(0.2)   # Wait for 1 second
+
+
+_thread.start_new_thread(loop, ())
+
+
